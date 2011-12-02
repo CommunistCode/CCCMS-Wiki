31d561de9875aaa97f8d425ccee1122f02c2ac10
Jack Mitchell <jack@communistcode.co.uk>
2011-12-02 14:26:25 +0000

    give create page a title

--- createPage.php
+++ createPage.php
@@ -6,6 +6,7 @@
 	require_once($fullPath."/wiki/classes/wikiPage.class.php");
 	require_once($fullPath."/includes/global.inc.php");
 
+	$pageTitle = "Create New Wiki Page";
 	$heading = "Create New Page";
 	$include = "includes/createPage.inc.php";
 
